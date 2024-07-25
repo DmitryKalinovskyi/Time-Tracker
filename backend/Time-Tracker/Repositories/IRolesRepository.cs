4d6ffda823fc37bb4fb6e473b4a31aa4ada63295
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-07-25 15:31:40 +0300

    Roles Repository

--- backend/Time-Tracker/Repositories/IRolesRepository.cs
+++ backend/Time-Tracker/Repositories/IRolesRepository.cs
@@ -5,5 +5,11 @@ namespace Time_Tracker.Repositories
     public interface IRolesRepository
     {
         public Role? Find(int roleId);
+
+        public int Insert(Role role);
+
+        public void Update(int roleId, Role role);
+        
+        public void Delete(int roleId);
     }
 }
