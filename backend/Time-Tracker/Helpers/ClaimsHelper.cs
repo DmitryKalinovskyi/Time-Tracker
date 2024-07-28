6b5750045b9ce3cb635acab84ad4de7cda46a176
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-07-28 10:17:49 +0300

    ClaimHelper possible errors fix

--- backend/Time-Tracker/Helpers/ClaimsHelper.cs
+++ backend/Time-Tracker/Helpers/ClaimsHelper.cs
@@ -5,9 +5,13 @@ namespace Time_Tracker.Helpers
 {
     public static class ClaimsHelper
     {
-        public static int? GetUserId(this ClaimsPrincipal claimsPrincipal)
+        public static int? GetUserId(this ClaimsPrincipal? claimsPrincipal)
         {
-            if(int.TryParse(claimsPrincipal.Claims.First().Value, out int userId))
+            if (claimsPrincipal == null) return null;
+
+            var claim = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            if (claim != null && int.TryParse(claim.Value, out int userId))
                 return userId;
 
             return null;
