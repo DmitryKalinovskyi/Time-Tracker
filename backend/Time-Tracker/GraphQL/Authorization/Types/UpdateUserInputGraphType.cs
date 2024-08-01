7fe39e6a7bf9231bb8d324d5edb70cf69766db94
Anton Ovod <antovod36@gmail.com>
2024-08-01 15:42:21 +0200

    Minor changes in update user input graph type

--- backend/Time-Tracker/GraphQL/Authorization/Types/UpdateUserInputGraphType.cs
+++ backend/Time-Tracker/GraphQL/Authorization/Types/UpdateUserInputGraphType.cs
@@ -1,6 +1,16 @@
-﻿namespace Time_Tracker.GraphQL.Authorization.Types
+﻿using GraphQL.Types;
+
+namespace Time_Tracker.GraphQL.Authorization.Types
 {
-    public class UpdateUserInputGraphType
+    public class UpdateUserInputGraphType : InputObjectGraphType
     {
+
+        public UpdateUserInputGraphType()
+        {
+            Field<NonNullGraphType<IntGraphType>>("id");
+            Field<StringGraphType>("fullName");
+            Field<StringGraphType>("email");
+            Field<IntGraphType>("roleId");
+        }
     }
 }
