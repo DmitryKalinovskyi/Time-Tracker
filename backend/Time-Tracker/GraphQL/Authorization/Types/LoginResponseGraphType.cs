1a094f02b21909cdd16b10ad37178e1d30e8fa34
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-08-05 11:01:42 +0300

    Modify LoginResponseGraphType to return user alongside with userId

--- backend/Time-Tracker/GraphQL/Authorization/Types/LoginResponseGraphType.cs
+++ backend/Time-Tracker/GraphQL/Authorization/Types/LoginResponseGraphType.cs
@@ -1,14 +1,17 @@
 ﻿using GraphQL.Types;
 using Microsoft.AspNetCore.Identity.Data;
 using Time_Tracker.Dtos;
+using Time_Tracker.Repositories;
 
 namespace Time_Tracker.GraphQL.Authorization.Types;
 
 public class LoginResponseGraphType : ObjectGraphType<LoginResponseDto>
 {
-    public LoginResponseGraphType()
+    public LoginResponseGraphType(IUsersRepository usersRepository)
     {
         Field(l => l.UserId, nullable: false);
         Field(l => l.AccessToken, nullable: true, type: typeof(TokenGraphType));
+        Field<NonNullGraphType<UserGraphType>>("user")
+            .Resolve(context => usersRepository.Find(context.Source.UserId));
     }
 }
