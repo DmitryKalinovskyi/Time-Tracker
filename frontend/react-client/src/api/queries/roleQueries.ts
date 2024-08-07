7902ea0789bb417a4c738042ab76d7a749936a9a
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-08-07 12:35:15 +0300

    Add graphql request for the role queries

---
+++ frontend/react-client/src/api/queries/roleQueries.ts
@@ -0,0 +1,70 @@
+import Role from "../../app/types/Role.ts";
+export interface createRoleResponse{
+    rolesMutation: {
+        createRole: {
+            id: number,
+            name: string,
+            permissions: string[]
+        }
+    }
+}
+
+export const createRoleQuery = (role: Role
+) => `
+    mutation CreateRole{
+      rolesMutation{
+        createRole(role: {
+          name: "${role.name}",
+          permissions: ${role.permissions}
+        }){
+          id,
+          name,
+          permissions
+        }
+      }
+    }
+    `;
+
+export interface updateRoleResponse{
+    rolesMutation: {
+        updateRole: {
+            id: number,
+            name: string,
+            permissions: string[]
+        }
+    }
+}
+
+export const updateRoleQuery = (role: Role
+) => `
+    mutation UpdateRole{
+      rolesMutation{
+          updateRole(role:{
+            id: ${role.id},
+            name: "${role.name}",
+            permissions: ${role.permissions}
+          }){
+            id,
+            name,
+            permissions
+          }
+      }
+    }
+    `;
+
+export interface deleteRoleResponse{
+    rolesMutation: {
+        deleteRole: string
+    }
+}
+
+export const deleteRoleQuery = (id: number
+) => `
+    mutation DeleteRole{
+        rolesMutation{
+          deleteRole(id: ${id})
+        }
+    }
+    `;
+
+
