914992a992581be736678edd0f4d1850d1898845
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-08-09 14:32:32 +0300

    Moved sidebar, header and footer to the root

--- frontend/react-client/src/app/App.tsx
+++ frontend/react-client/src/app/App.tsx
@@ -1,33 +1,39 @@
 import LoginPage from "../app/ui/pages/LoginPage.tsx";
 import AccountVerificationPage from "../app/ui/pages/AccountVerificationPage.tsx";
-import {Provider} from "react-redux";
-import {store} from "./store.ts";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import NotFoundPage from "./ui/pages/NotFoundPage.tsx";
 import HomePage from "./ui/pages/HomePage.tsx";
 import RegisterUserPage from "./ui/pages/RegisterUserPage.tsx";
 import RequireAuth from "./gates/RequireAuth.tsx";
+import Root from "./ui/components/Root.tsx";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        errorElement: <NotFoundPage/>,
-        children: [
-            {element: <RequireAuth/>, children: [
-                { path: "/", element: <HomePage /> },
-                { path: "/home", element: <HomePage /> },
-                { path: "/register", element: <RegisterUserPage /> },
-                ]},
-            { path: "/login", element: <LoginPage /> },
-            { path: "/verification", element: <AccountVerificationPage /> },
-        ]
-    }
+  {
+    path: '/',
+    errorElement: <NotFoundPage />,
+    children: [{
+      element: <Root />, children: [
+        {
+          element: <RequireAuth />, children: [
+            { path: "/", element: <HomePage /> },
+            { path: "/home", element: <HomePage /> },
+            { path: "/register", element: <RegisterUserPage /> },
+          ]
+        }
+      ]
+    },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/verification", element: <AccountVerificationPage /> },
+    ]
+  }
 ])
 
 export default function App() {
-    return (
-        <Provider store={store}>
-            <RouterProvider router={router}/>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 }
--- frontend/react-client/src/app/ui/pages/HomePage.tsx
+++ frontend/react-client/src/app/ui/pages/HomePage.tsx
@@ -1,29 +1,13 @@
-import useIsAuthenticated from "../../hooks/useIsAuthenticated.ts";
-import {Navigate} from "react-router-dom";
-import Sidebar from '../components/Sidebar.tsx';
-import { Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import TimeTracker from "../components/TimeTracker.tsx";
-import Navbar from "../components/Navbar.tsx";
-import Footer from "../components/Footer.tsx";
 
-export default function HomePage(){
-    return (
-        <>
-          <CssBaseline />
-          <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <Box sx={{ display: 'flex', flexGrow: 1 }}>
-              <Sidebar />
-              <Box component="main" sx={{ flexGrow: 1, p: 3 }} className="bg-gray-100 min-h-screen">
-                <Toolbar />
-                <Typography variant="h4" className="text-blue-800" sx={{mb: 4}}>
-                  Welcome to Time Tracker
-                </Typography>
-                <TimeTracker />
-              </Box>
-            </Box>
-            <Footer />
-          </div>
-        </>
-      );
-    };
+export default function HomePage() {
+  return (
+    <>
+      <Typography variant="h4" className="text-blue-800" sx={{ mb: 4 }}>
+        Welcome to Time Tracker
+      </Typography>
+      <TimeTracker />
+    </>
+  );
+};
