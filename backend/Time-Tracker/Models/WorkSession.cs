a93883670e72c8efaa30c2e8c06d86d4163056d3
Anton Ovod <antovod36@gmail.com>
2024-08-11 15:18:52 +0200

    Model for WorkSession object has been implemented

---
+++ backend/Time-Tracker/Models/WorkSession.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Identity.Client;
+
+namespace Time_Tracker.Models
+{
+    public class WorkSession
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public int SessionOriginId { get; set; }
+
+        public long? Duration { get; set; }
+
+        public int? EditedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
