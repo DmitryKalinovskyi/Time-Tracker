84b9e074e0dcfc8c61fd38c5c95bbe395848f72d
Anton Ovod <antovod36@gmail.com>
2024-08-11 15:57:22 +0200

    First test version of iworksessionrepo has been implemented

---
+++ backend/Time-Tracker/Repositories/IWorkSessionsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Time_Tracker.Models;
+
+namespace Time_Tracker.Repositories
+{
+    public interface IWorkSessionsRepository
+    {
+        Task<int> AddWorkSessionAsync(WorkSession workSession);
+
+        Task UpdateWorkSessionAsync(WorkSession workSession);
+
+        Task DeleteWorkSessionAsync(int id);
+
+        Task<WorkSession> GetWorkSessionByIdAsync(int id);
+
+        Task<List<WorkSession>> GetWorkSessionsWithSortingAsync(int? first, int? last, int? before, int? after);
+
+
+    }
+}
