b2931dfde7ab04992e4d94123edfd5cabd06c7f5
Anton Ovod <antovod36@gmail.com>
2024-08-11 20:14:47 +0200

    Added endpoint for getting specific work session by id

---
+++ backend/Time-Tracker/GraphQL/TimeTracking/Queries/TimeTrackerQuery.cs
@@ -0,0 +1,25 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Time_Tracker.GraphQL.TimeTracking.Types;
+using Time_Tracker.Repositories;
+
+namespace Time_Tracker.GraphQL.TimeTracking.Queries
+{
+    public class TimeTrackerQuery: ObjectGraphType
+    {
+        public TimeTrackerQuery(IWorkSessionRepository workSessionRepository) 
+        {
+            Field<WorkSessionGraphType>("getWorkSessionById")
+                .Argument<IntGraphType>("id")
+                .ResolveAsync(async context =>
+                {
+                    var inputId = context.GetArgument<int>("id");
+
+                    var workSession = await workSessionRepository.GetWorkSessionByIdAsync(inputId);
+
+                    return workSession;
+                });
+
+        }
+    }
+}
