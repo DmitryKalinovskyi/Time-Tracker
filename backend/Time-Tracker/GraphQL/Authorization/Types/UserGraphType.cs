befef8abb50842120bd9848f4028fd4aa14c5013
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-09-04 22:18:19 +0300

    Added calendar events to the user graph type

--- backend/Time-Tracker/GraphQL/Authorization/Types/UserGraphType.cs
+++ backend/Time-Tracker/GraphQL/Authorization/Types/UserGraphType.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.Types;
+using Time_Tracker.GraphQL.Calendar.Types;
 using Time_Tracker.Models;
 using Time_Tracker.Repositories;
 
@@ -6,12 +7,15 @@ namespace Time_Tracker.GraphQL.Authorization.Types;
 
 public class UserGraphType : ObjectGraphType<User>
 {
-    public UserGraphType()
+    public UserGraphType(ICalendarEventsRepository calendarEventsRepository)
     {
         Field(t => t.Id);
         Field(t => t.FullName);
         Field(t => t.Email);
         Field(t => t.Permissions, nullable: true);
         Field(t => t.IsActive);
+
+        Field<NonNullGraphType<ListGraphType<NonNullGraphType<CalendarEventGraphType>>>>("calendarEvents")
+            .ResolveAsync(async context => await calendarEventsRepository.FindAllByUserIdAsync(context.Source.Id));
     }
 }
